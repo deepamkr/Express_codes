0ab8a3eac4770c7edc5c1a532863ee9b58d914af
deepamkr <deepamkumar@quaratech.com>
2023-01-23 17:13:17 +0530

    Express_codes

---
+++ public/members.js
@@ -0,0 +1,22 @@
+const members=[{
+    id:1,
+    name: 'A',
+    email: 'A@gmail.com'
+    },
+    {   id:2,
+        name: 'B',
+        email: 'B@gmail.com'
+
+    },
+    {   id:3,
+        name: 'C',
+        email: 'C@gmail.com'
+
+    },
+    {   id:4,
+        name: 'D',
+        email: 'D@gmail.com'
+
+    }
+]
+module.exports= members
