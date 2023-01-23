0a04e194393aacc3688c048a5e37af264fa13fb9
deepamkr <deepamkumar@quaratech.com>
2023-01-23 17:15:09 +0530

    Express_codes

--- storage.js
+++
@@ -1,5 +0,0 @@
-//to send the html as response by mentioning the path
-app.get('',(req,res)=>{
-    //res.send('<h1>EXPRESS JS</h1>')
-    res.sendFile(path.join(__dirname,'public','index.html'))
-})
